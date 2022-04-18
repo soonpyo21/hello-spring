5b4127bbaf4e85053d25bf21e2beda1dac3467c2
soonpyo21 <tnsvy2004@gmail.com>
2022-04-18 23:34:32 +0900

    AOP가 필요한 상황

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,9 +23,18 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -39,7 +48,16 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() { // repository의 메소드와는 다르게 service 클래스는 비지니스에 가까운 용어를 써야한다.
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
 
     /**
